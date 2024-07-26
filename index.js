e04520b9aa0783e533a91e8822b1ca34d0f67119
migue.centellas@gmail.com <migue.centellas@gmail.com>
2024-07-26 16:51:33 -0300

    hasta 27-crea server node- luego de arrancar probar en browser http://localhost:3900/

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const {conexion} = require("./database/conexion");
-
+const express = require("express");
+const cors = require("cors");
 console.log("arranque -");
-
+//conexion db
 conexion();
+//crear servidor node
+const app = express();
+const puerto= 3900;
+//confuguro cors
+app.use(cors()); //ejecuta cors antes que las rutas
+
+//convrtir body a objeto js
+app.use(express.json());
+//creo rutas
+
+//crea servidor y escucha peticiones http
+app.listen(puerto,()=> {
+    console.log("servido corriend en puerto "+puerto);
+});
